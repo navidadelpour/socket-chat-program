fcdfa24146881eabf6388e78ff8bf2e73cf7fa74
Navid <navidadelpour@gmail.com>
2019-08-24 11:18:50 +0430

    user connect and disconnect implemented

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var connected_users = []
+
 io.on('connection', function(socket){
+  let username = ""
+  socket.on('user nickname', function(name){
+    socket.emit('connected', connected_users)
+    io.emit('user connected', name)
+    connected_users.push(name)
+    username = name
+  })
+  socket.on('disconnect', function() {
+    io.emit('user disconnected', username)
+    connected_users.splice(connected_users.indexOf(username), 1)
+  }) 
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
@@ -16,3 +30,11 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+// Broadcast a message to connected users when someone connects or disconnects.
+// Add support for nicknames.
+// Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+// Add “{user} is typing” functionality.
+// Show who’s online.
+// Add private messaging.
+// Share your improvements!
